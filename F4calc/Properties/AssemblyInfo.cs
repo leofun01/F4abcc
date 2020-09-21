57b90d470376b8209b1cd6fb87839297f6420816
leofun01 <leofun01@gmail.com>
2020-09-22 01:18:06 +0300

    Init VS project

---
+++ F4calc/Properties/AssemblyInfo.cs
@@ -0,0 +1,16 @@
+using System.Reflection;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("F4calc")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("F4calc")]
+[assembly: AssemblyCopyright("Copyleft (cc) leofun01 2020")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+[assembly: ComVisible(false)]
+[assembly: Guid("133262dc-00b5-4297-b2a4-f2477d2dcc94")]
+[assembly: AssemblyVersion("0.1.0.0")]
+[assembly: AssemblyFileVersion("0.1.0.0")]
