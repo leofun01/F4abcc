b8701c6c005f7496a95c9f524017d316775d20d1
leofun01 <leofun01@gmail.com>
2020-10-24 23:22:02 +0300

    Add triangular cut for series.

--- F4calc/Series.cs
+++ F4calc/Series.cs
@@ -4,7 +4,7 @@ namespace F4calc
 {
 	public static class Series
 	{
-		public static double F4Series(
+		public static double F4SeriesRectangle(
 			int a, int b, int c1, int c2,
 			double z1, double z2, int m, int n)
 		{
@@ -20,7 +20,7 @@ namespace F4calc
 			}
 			return sum;
 		}
-		public static double F4Series(
+		public static double F4SeriesSquare(
 			int a, int b, int c1, int c2,
 			double z1, double z2, double eps,
 			int nLimit, out int n)
@@ -29,7 +29,40 @@ namespace F4calc
 			double r0, r1 = 0d;
 			do {
 				r0 = r1;
-				r1 = F4Series(a, b, c1, c2, z1, z2, ++n, n);
+				r1 = F4SeriesRectangle(a, b, c1, c2, z1, z2, ++n, n);
+			} while(Math.Abs(r0 - r1) > eps && n <= nLimit);
+			if(n > nLimit
+				|| double.IsNaN(r1)
+				|| double.IsInfinity(r1)
+			) n = -1;
+			return r1;
+		}
+		public static double F4SeriesTriangle(
+			int a, int b, int c1, int c2,
+			double z1, double z2, int m)
+		{
+			if(m < 0) return 1d;
+			double sum = 0d, p1 = 1d, p2;
+			for(int i = 0; i <= m; ) {
+				p2 = p1;
+				for(int j = 0; j <= m - i; ) {
+					sum += p2;
+					p2 *= z2 * (a + i + j) * (b + i + j) / ((c1 + j) * ++j);
+				}
+				p1 *= z1 * (a + i) * (b + i) / ((c1 + i) * ++i);
+			}
+			return sum;
+		}
+		public static double F4SeriesTriangle(
+			int a, int b, int c1, int c2,
+			double z1, double z2, double eps,
+			int nLimit, out int n)
+		{
+			n = -1;
+			double r0, r1 = 0d;
+			do {
+				r0 = r1;
+				r1 = F4SeriesTriangle(a, b, c1, c2, z1, z2, ++n);
 			} while(Math.Abs(r0 - r1) > eps && n <= nLimit);
 			if(n > nLimit
 				|| double.IsNaN(r1)
